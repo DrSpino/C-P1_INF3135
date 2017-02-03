2c5b935bad032012a9b53d1a3911077c0ab68daf
DrSpino <hicham_diouri@outlook.com>
2017-02-03 00:42:56 -0500

    Méthode validation du seuil

--- tp1.c
+++ tp1.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>                                                     
 
+void valideSeuil(int taille, double seuil[]);
+
 int main(int argc, char *argv[]){                                                                               
 
 	if(argc != 12){                                                                                                                 
@@ -8,21 +10,25 @@ int main(int argc, char *argv[]){
 		exit(1);
 	}
 
-	double bareme [11] = {atof(argv[1]),atof(argv[2]),atof(argv[3]),
+	double seuil [11] = {atof(argv[1]),atof(argv[2]),atof(argv[3]),
 			atof(argv[4]),atof(argv[5]),atof(argv[6]), 
 			atof(argv[7]),atof(argv[8]),atof(argv[9]), 
 			atof(argv[10]),atof(argv[11])};     
 
-	int i;
-	for(i = 1; i < argc-1; i++){
-		int n;
-		for(n = i+1; n < argc; n++){
-			if(bareme[i] <= bareme[n]){
-				printf("ERRREUR 2\n");
-				exit(1);
-			}
-		}
-	}
-
+	valideSeuil(argc,seuil);
+	
 	return 0;
+}
+
+void valideSeuil(int taille, double seuil[]){
+	int i;                                                                                                                                                                  
+	for(i = 1; i < taille-1; i++){                                                                                                                                                    
+		int n;                                                                                                                                                                  
+		for(n = i+1; n < taille; n++){                                                                                                                                                    
+			if(seuil[i] <= seuil[n]){                                                                                                                                                       
+				printf("ERRREUR 2\n");                                                                                                                                                  
+				exit(1);
+			} 
+		}            
+	}
 }
